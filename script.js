a92803fe5ec2aed74a4661b0c92c462c22234b66
AlissonSanita <00001115240961sp@al.educacao.sp.gov.br>
2024-08-26 12:29:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const next = document.querySelectorAll('.btn-proximo');
 
+next.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const nextStep = 'passo-' + this.getAttribute('data-proximo');
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
